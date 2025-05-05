55c88c6f311175a2ee2c41bdd7745f0f1366d2c7
yeeE1233 <412737099@o365.tku.edu.tw>
2025-05-05 11:57:50 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+let poses = [];
+let img;
+
+function preload() {
+  // 預載圖片
+  img = loadImage('UoKfhYw.png');
+}
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded); //呼叫在ml5.js上的net函數，用此函數來判斷各位置，呼叫成功即執行function modelLoaded 
+  poseNet.on('pose', function (results) {
+    poses = results;
+  });
+}
+
+function gotPoses(poses) {
+  //console.log(poses);
+  if (poses.length > 0) {
+    pose = poses[0].pose;  //把抓到的幾個點，都放置pose變數內
+    skeleton = poses[0].skeleton; //把相關於骨架的點都放到skeleton變數內
+  }
+}
+
+
+function modelLoaded() {   //顯示pose model已經準備就緒
+  console.log('PoseNet 已載入');
+}
+
+function draw() {
+    background(220);
+		translate(video.width,0)  //因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
+		scale(-1,1)    //因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
+    image(video, 0, 0, width, height);  //顯示你的畫面在螢幕上
+		if (pose) {
+			let eyeR = pose.rightEye;  //抓到右眼資訊，放到eyeR
+			let eyeL = pose.leftEye;   //抓到左眼資訊，放到eyeL
+			let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y); //算出左右眼的距離，當作鼻子顯示圓的直徑
+			fill(255, 0, 0);
+			ellipse(pose.nose.x, pose.nose.y, d); //畫出鼻子的圓
+			fill(0, 0, 255);
+			ellipse(pose.rightWrist.x, pose.rightWrist.y, 62); //畫出右手腕圓圈
+			ellipse(pose.leftWrist.x, pose.leftWrist.y, 62); //畫出左手腕圓圈
+			drawKeypoints()
+			drawSkeleton()
+ 		}
+    // 如果偵測到姿勢，檢查左耳的位置
+    if (poses.length > 0) {
+      const pose = poses[0].pose;
+      const leftEar = pose.keypoints[3]; // 左耳的 keypoint
+
+      if (leftEar && leftEar.score > 0.5) {
+        // 顯示圖片在左耳的位置
+        image(img, leftEar.position.x, leftEar.position.y, 50, 50); // 調整圖片大小為 50x50
+      }
+    }
+}
+function drawKeypoints()  {  
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;//找出每一個點的x座標
+      let y = pose.keypoints[i].position.y;//找出每一個點的y座標
+      fill(0,255,0);
+      ellipse(x,y,16,16);
+    }
+	//print(pose.keypoints.length)
+	}
+function drawSkeleton()  {
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];			
+      strokeWeight(2);
+      stroke(255,0,0);
+      line(a.position.x, a.position.y,b.position.x,b.position.y);			
+    }
+  }
+
